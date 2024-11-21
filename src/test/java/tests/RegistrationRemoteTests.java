c2aa8b23e52e119a46322f5efd0479af55d8be3d
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-21 21:56:25 +0300

    Added remote demoqa test

---
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -0,0 +1,86 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import pages.TextBoxPage;
+
+public class RegistrationRemoteTests {
+
+    TestData testData = new TestData();
+    RegistrationPage registrationPage = new RegistrationPage();
+    TextBoxPage textBoxPage = new TextBoxPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserPosition = "0x0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    @Tag("demoqa")
+    void successRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setEmail(testData.userEmail)
+                .setGender(testData.userGender)
+                .setPhone(testData.userNumber)
+                .setBirthDate(testData.userBirthDay, testData.userBirthMonth, testData.userBirthYear)
+                .setSubject(testData.subject)
+                .setHobby(testData.hobby)
+                .uploadPicture(testData.picturePath)
+                .setCurrentAddress(testData.currentAddress)
+                .setStateAndCity(testData.state, testData.city);
+
+        registrationPage.submitRegistrationForm();
+
+        registrationPage
+                .verifyResultsModal()
+                .verifyResult("Student Name", testData.firstName + " " + testData.lastName)
+                .verifyResult("Student Email", testData.userEmail)
+                .verifyResult("Gender", testData.userGender)
+                .verifyResult("Mobile", testData.userNumber)
+                .verifyResult("Date of Birth", testData.userBirthDay + " " + testData.userBirthMonth + ","
+                        + testData.userBirthYear)
+                .verifyResult("Subjects", testData.subject)
+                .verifyResult("Hobbies", testData.hobby)
+                .verifyResult("Picture", testData.pictureName)
+                .verifyResult("Address", testData.currentAddress)
+                .verifyResult("State and City", testData.state + " " + testData.city);
+    }
+
+    @Test
+    void minimalRequiredRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setGender(testData.userGender)
+                .setPhone(testData.userNumber);
+
+        registrationPage.submitRegistrationForm();
+
+        registrationPage.verifyResultsModal()
+                .verifyResult("Student Name", testData.firstName + " " + testData.lastName)
+                .verifyResult("Gender", testData.userGender)
+                .verifyResult("Mobile", testData.userNumber);
+    }
+
+    @Test
+    void invalidEmailRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setEmail(testData.invalidEmail)
+                .setGender(testData.userGender)
+                .setPhone(testData.userNumber);
+
+        registrationPage.submitRegistrationForm();
+
+        registrationPage.verifyEmailErrorNotification();
+    }
+}
