ee7c317ffa2e5a82dfb42834f7fa8de50dfed835
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-21 23:03:48 +0300

    Add allure listener

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import pages.RegistrationPage;
 import pages.TextBoxPage;
@@ -17,5 +19,7 @@ public class BaseTest {
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
--- src/test/java/tests/RegistrationRemoteTests.java
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,8 @@ public class RegistrationRemoteTests {
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @Test
